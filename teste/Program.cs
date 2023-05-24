b60b95e8e2233950748db5ecdee41c27d645996c
Gustavo Silvério <gustavosk290905@gmail.com>
2023-05-23 22:40:19 -0300

    Correcao dnv

--- teste/Program.cs
+++ teste/Program.cs
@@ -16,41 +16,6 @@ builder.Services.AddHttpContextAccessor();
 // Configure the HTTP request pipeline.
 var app = builder.Build();
 
-// Setup database
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-using (var connection = new SqlConnection(connectionString))
-{
-    connection.Open();
-    var command = connection.CreateCommand();
-
-    // Cria as tabelas se elas não existirem, PARA FUNCIONAR É PRECISO QUE CRIE UM BANCO DE DADOS COM O NOME QUE QUISER E DEPOIS COLOCAR ESSE NOME
-    // NOME NA CONNECTION_STRING,E NA QUERY ABAIXO ANTES DE INICIAR O PROJETO
-    command.CommandText = @"
-        USE teste123;
-
-        IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'Usuario')
-        BEGIN
-            CREATE TABLE Usuario (
-                Id INT PRIMARY KEY IDENTITY(1,1),
-                Nome NVARCHAR(255) NOT NULL,
-                Email NVARCHAR(255) NOT NULL
-            );
-        END;
-
-        IF NOT EXISTS(SELECT * FROM sys.tables WHERE name = 'Tarefas')
-        BEGIN
-            CREATE TABLE Tarefas (
-                Id INT PRIMARY KEY IDENTITY(1,1),
-                IdUsuario INT NOT NULL,
-                Titulo NVARCHAR(255) NOT NULL,
-                Descricao NVARCHAR(MAX) NOT NULL,
-                FOREIGN KEY (IdUsuario) REFERENCES Usuario(Id)
-            );
-        END;
-    ";
-    command.ExecuteNonQuery();
-}
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
